e053a9fccfaa525b7839c6ad8931232637aa7274
Steve Kramer <steve@newknowledge.io>
2019-03-25 13:34:08 -0500

    Updating spacy requirement to 1.2.0 for d3m_ibex version 1.1.3.

--- d3m_ibex/__init__.py
+++ d3m_ibex/__init__.py
@@ -1,6 +1,6 @@
 from d3m_ibex.d3m_ibex import Ibex
 
-__version__ = '1.1.2'
+__version__ = '1.1.3'
 
 __all__ = [
           'Ibex'
--- setup.py
+++ setup.py
@@ -9,15 +9,17 @@ from setuptools.command.install import install
 
 setup(
     name='d3m_ibex',
-    version='1.1.2',
+    version='1.1.3',
     description='Named entity extraction',
     author='New Knowledge',
     packages=['d3m_ibex'],
     package_data={'d3m_ibex': ['exclude_words.txt']},
     include_package_data=True,
     install_requires=[
-        'spacy==1.7.0',
+        'spacy==2.1.0',
         'flask>=1.0.2',
-        'nose>=1.3.7'
+        'nose>=1.3.7',
+        'https://github.com/explosion/spacy-models/releases/download/en_core_web_md-2.1.0/en_core_web_md-2.1.0.tar.gz',
+        'https://github.com/explosion/spacy-models/releases/download/es_core_news_md-2.1.0/es_core_news_md-2.1.0.tar.gz'
     ],
 )
